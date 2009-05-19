4e5202fb391fd24e86a65f4248a00ae49294d518
daniel g. siegel <siegel@in.tum.de>
2009-05-19 15:52:02 +0200

    add the working api, created src directory

---
+++ src/api.py
@@ -0,0 +1,96 @@
+# python twitter tools (http://mike.verdone.ca/twitter/) by
+# Mike Verdone was used as a reference for this API, thanks!
+
+import sys
+import os
+
+from base64 import b64encode
+from urllib import urlencode
+import urllib2
+import json
+
+from exceptions import Exception
+
+TWITTER_API_METHODS_POST = [
+    "create",       # in blocks/
+                    #    favorites/
+                    #    friendships/
+
+    "destroy",      # in blocks/
+                    #    direct_messages/
+                    #    favorites/
+                    #    friendships/
+                    #    statuses/
+
+    "follow",       # in notifications
+    "leave",
+
+    "new",          # in direct_messages/
+
+    "update",       # in statuses/
+
+    "end_session",  # in account/
+    "update_delivery_device",
+    "update_profile",
+    "update_profile_background_image",
+    "update_profile_colors",
+    "update_profile_image",
+]
+
+def dump (obj):
+  print json.dumps(obj, sort_keys=True, indent=2)
+
+class TwitterAPICall (object):
+  def __init__ (self, user=None, password=None, domain="twitter.com", uri=""):
+    self.user = user
+    self.password = password
+    self.domain = domain
+    self.uri = uri
+    self.format = "json"
+
+  def __getattr__ (self, arg):
+    try:
+      return object.__getattr__ (self, arg)
+    except AttributeError:
+      return TwitterAPICall (self.user, self.password, self.domain,
+                             self.uri + "/" + arg)
+  def __call__ (self, **args):
+    method = "GET"
+    for action in TWITTER_API_METHODS_POST:
+        if self.uri.endswith (action):
+            method = "POST"
+            break
+
+    uri = self.uri
+    if ("id" in args):
+      uri += "/%s" % args.pop("id")
+
+    headers = {}
+    query = ""
+    post = None
+
+    if (method == "GET"):
+      if (args):
+        query = "?%s" %(urlencode(args.items()))
+    else:
+      post = urlencode(args.items())
+
+    if (self.username):
+      headers["Authorization"] = "Basic " + b64encode ("%s:%s" %(self.user, self.password))
+
+    url = urllib2.Request ("http://%s%s.%s%s" %(self.domain, uri, self.format, query),
+                           post, headers)
+    try:
+      handle = urllib2.urlopen(url)
+      return json.loads(handle.read())
+    except urllib2.HTTPError, e:
+      raise TwitterAPIError ("HTTP response code %i on URL: %s.%s using parameters: (%s)\ndetails: %s"
+                          %(e.code, uri, self.format, urlencode(args.items()), e.fp.read()))
+
+class TwitterAPIError (Exception):
+  pass
+
+class TwitterAPI (TwitterAPICall):
+  def __init__ (self, user=None, password=None, domain="twitter.com"):
+    TwitterAPICall.__init__(self, user, password, domain, "")
+
