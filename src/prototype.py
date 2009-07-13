48c6907fcd9a305668a90d7a5d806c3ef957fe02
daniel g. siegel <siegel@in.tum.de>
2009-07-13 13:19:21 +0200

    [src] add command line options

--- src/prototype.py
+++ src/prototype.py
@@ -5,12 +5,13 @@ import os
 #sys.path.append(os.getcwd()+ "/")
 
 from base64 import b64encode
-from urllib import urlencode
+from urllib import urlencode, quote_plus
 import urllib2
 import json
 import time
 import codecs
 import webbrowser
+from optparse import OptionParser
 
 from exceptions import Exception
 
@@ -18,7 +19,7 @@ import markup
 from markup import oneliner as e
 
 try:
-  from twitterapi import TwitterAPI, dump
+  from twitterapi import TwitterAPI, dump, TwitterAPIError
   from plugin import Plugin, init_plugin_system, find_plugins
   import conf
 
@@ -26,13 +27,51 @@ except ImportError:
   print "The prototype modules can not be found."
   sys.exit(1)
 
+
+parser = OptionParser()
+parser.add_option("-u", "--user", help="analyze a Twitter username")
+parser.add_option("-k", "--keyword", help="search for a specific keyword")
+parser.add_option("-l", "--location", help="search for a location")
+parser.add_option("-r", "--range", help="a range in kilometers (has to be used together with the location option), e.g. 15")
+
+(options, args) = parser.parse_args()
+
 twitter = TwitterAPI(conf.username, conf.password)
 
-init_plugin_system()
+if options.user:
+  try:
+    id = twitter.users.show(id=options.user, no_cache=True)
+  except (TwitterAPIError, ValueError):
+    print "No user named '%s' found. Running a search now." % options.user
+    print "Please re-run this program, once you have found a user"
+    webbrowser.open("http://search.twitter.com/search?q=\"%s\"" % quote_plus(options.user))
+    sys.exit()
+elif options.keyword:
+  print "Running a keyword search for '%s'" % options.keyword
+  print "Please re-run this program, once you have found a user"
+  webbrowser.open("http://search.twitter.com/search?q=\"%s\"" % quote_plus(options.keyword))
+  sys.exit()
+elif options.location:
+  if not options.range:
+    options.range = "5"
+  print "Running a location search for '%s' within %s km" % (options.location, options.range)
+  print "Please re-run this program, once you have found a user"
+  webbrowser.open("http://search.twitter.com/search?near=\"%s\"&within=%s&units=km"
+      % (quote_plus(options.location), options.range))
+  sys.exit()
+elif options.range:
+  print "Option range has to be used together with the location option"
+  parser.print_help()
+  exit(-1)
+else:
+  parser.print_help()
+  exit(-1)
 
-id = "furukama"
+
+id = str(options.user)
 data = {"id": id}
-#data = None
+
+init_plugin_system()
 
 date = "Created on %s" % time.asctime()
 title = "Fact Sheet"
